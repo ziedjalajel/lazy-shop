5bfbd543d547f1189e6558231d9186e990976102
ziedjalajel <zied.jalajel1993@gmail.com>
2021-06-01 17:43:19 +0300

    task2

--- lazyshop/src/App.js
+++ lazyshop/src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';//this is how to import logos&images
+import products from "./products"
+import Home from "./Components/Home"
 import './App.css';//this is how to add cdd
 
 function App() {
+  const newArr = products.map((product)=>(
+  <div> 
+    <p><img className="productimage" src={product.image}/></p>
+    <p>{product.name}</p>
+    <p>{product.price} JD</p>
+   
+  </div>
+  ));
+
   return (//this what will render on the screen . and it's not html it's called jsx(javascript XML) and you can right js in it 
     <div className="title">
-    
-        
-        <h1 className="h1">
-         Lazy Chairs for Lazy US 
-        </h1>
-        <h5 className="h2">
-          One more action for the lazy life you deserve .
-        </h5>
-        <img className="App-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu3Tzrr666L7QiUIlBV7Aw132gtFLlcdyUgg&usqp=CAU"  alt="logo" />
+    <Home/>
+         
+         <div class="narray">{newArr}</div> 
+          
         
     
     </div>
---
+++ lazyshop/src/Components/Home.js
@@ -0,0 +1,13 @@
+
+const Home=()=>{
+  return(
+    <><h1 className="h1">
+    Lazy Chairs for Lazy US 
+   </h1>
+   <h5 className="h2">
+     One more action for the lazy life you deserve .
+   </h5>
+   <img className="App-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd7GNVjgdC0sOkcJlMuIp3zWrNu788TZfiRA&usqp=CAU"  alt="recliner" />
+   </>)
+}
+export default Home;
